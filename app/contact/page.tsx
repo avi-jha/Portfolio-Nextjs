19952ad02c37dc22683af03406d05dbb625860a7
Avinash Jha <avinash.jha@easebuzz.in>
2024-09-27 19:21:38 +0530

    changed contact page ui

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import JsonData from '../Data/data.json'
 import Link from 'next/link'
+import "../styles/customStyles.css";
 
 const ContactPage = () => {
     return (
@@ -18,20 +19,59 @@ const ContactPage = () => {
                 </div>
                 <div className=' items-start justify-center flex flex-col m-12 mt-20'>
                     <p className='text-xl loading-relaxed'>contact me.</p>
-                    <div className='w-60'>
-                        {JsonData.Contact.map((data, index) => (
-                            data === 'Message' ?
-                                <textarea className='w-80 mt-10 p-5 pt-7 pb-7 border rounded-lg border-stone-500 hover:border-white focus:border-white focus:outline-none placeholder-slate-400 text-white bg-transparent' placeholder='Message' key={data + index} />
-                                : <input
-                                    key={data + index}
+                    <div className='mt-10'>
+                        <div className='flex flex-row justify-between'>
+                            <div className='mr-5'>
+                                <p className='required-field'>First Name</p>
+                                <br />
+                                <input
                                     type="text"
-                                    className="w-80 h-10 mt-10 p-5 pt-7 pb-7 border rounded-lg border-stone-500 hover:border-white focus:border-white focus:outline-none placeholder-slate-400 text-white bg-transparent"
-                                    placeholder={data}
+                                    className="w-80 h-6 p-5 pt-6 pb-6 border border-stone-500 hover:border-white focus:border-white focus:outline-none placeholder-slate-400 text-white bg-transparent"
+                                    placeholder={'First Name'}
                                 />
-                        ))}
-                    </div>
-                    <div className='mt-20 w-32 border-white border flex justify-center rounded-lg hover:bg-white hover:text-black'>
-                        <button className='p-4'>Send</button>
+                            </div>
+                            <div className='ml-5'>
+                                <p className='required-field'>Last Name</p>
+                                <br />
+                                <input
+                                    type="text"
+                                    className="w-80 h-6 p-5 pt-6 pb-6 border border-stone-500 hover:border-white focus:border-white focus:outline-none placeholder-slate-400 text-white bg-transparent"
+                                    placeholder={'Last Name'}
+                                />
+                            </div>
+                        </div>
+                        <div className='flex flex-row justify-between mt-10'>
+                            <div className='mr-5'>
+                                <p className='required-field'>Email ID</p>
+                                <br />
+                                <input
+                                    type="text"
+                                    className="w-80 h-6 p-5 pt-6 pb-6 border border-stone-500 hover:border-white focus:border-white focus:outline-none placeholder-slate-400 text-white bg-transparent"
+                                    placeholder={'abc@xyz.com'}
+                                />
+                            </div>
+                            <div className='ml-5'>
+                                <p className='required-field'>Job Title</p>
+                                <br />
+                                <input
+                                    type="text"
+                                    className="w-80 h-6 p-5 pt-6 pb-6 border border-stone-500 hover:border-white focus:border-white focus:outline-none placeholder-slate-400 text-white bg-transparent"
+                                    placeholder={'What is your job title?'}
+                                />
+                            </div>
+                        </div>
+                        <div className='flex flex-row justify-between mt-10'>
+                            <div className='mr-5' >
+                                <p className='required-field'>Any Message or suggestion?</p>
+                                <br />
+                                <textarea className=' p-3 w-80 pt-7 pb-7 border border-stone-500 hover:border-white focus:border-white focus:outline-none placeholder-slate-400 text-white bg-transparent'
+                                    placeholder='Message'
+                                />
+                            </div>
+                        </div>
+                        <div className='mt-20 w-32 border-white border flex justify-center rounded-lg hover:bg-white hover:text-black'>
+                            <button className='p-4'>Send</button>
+                        </div>
                     </div>
                 </div>
             </div>
